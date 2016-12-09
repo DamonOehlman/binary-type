5ee910709bbf4f50684375f3998122007dc9053e
Oehlman <nathan.oehlman@gmail.com>
2016-12-09 15:40:10 +1100

    Prevent checking of the JPEG trailer, as some devices seem to create JPEGs without the appropriate trailer code

--- datablocks.js
+++ datablocks.js
@@ -10,7 +10,7 @@ module.exports = {
   image: {
     bmp: [ '42 4D' ],
     gif: [ '47 49 46 38 39 61', '00 3B' ], // gif89a at the moment
-    jpeg: ['FF D8 FF', 'FF D9' ],
+    jpeg: ['FF D8 FF' ],
     png: [ '89 50 4E 47 0D 0A 1A 0A', '49 45 4E 44 AE 42 60 82' ]
   },
 
